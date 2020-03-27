a19107c0085838f0b9b431ae5272dcab3bf6fbdf
Torleif West <torleifw@gmail.com>
2020-03-27 19:10:13 +0100

    Add a sprite based power meter sidebar.

--- OpenRA.Mods.Common/Widgets/Logic/Ingame/ClassicProductionLogic.cs
+++ OpenRA.Mods.Common/Widgets/Logic/Ingame/ClassicProductionLogic.cs
@@ -77,6 +77,7 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 				if (foreground != null)
 					foregroundTemplate = foreground.Get("ROW_TEMPLATE");
 
+				var powerMeter = widget.GetOrNull<SpritePowerMeterWidget>("POWER_BAR");
 				Action<int, int> updateBackground = (_, icons) =>
 				{
 					var rows = Math.Max(palette.MinimumRows, (icons + palette.Columns - 1) / palette.Columns);
@@ -93,6 +94,9 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 							row.Bounds.Y = i * rowHeight;
 							background.AddChild(row);
 						}
+
+						if (powerMeter != null)
+							powerMeter.Bounds.Height = rows * rowHeight;
 
 						if (backgroundBottom == null)
 							return;
